a55cc4d8fce006b40687b2c56841dca24d09f62d
Til Blechschmidt <til@blechschmidt.de>
2017-11-24 16:58:58 +0100

    Implemented Message class

--- modules/communication/Message.cpp
+++ modules/communication/Message.cpp
@@ -8,4 +8,104 @@
 
 namespace ProtoMesh::communication {
 
+    vector<uint8_t> Message::serialize() {
+
+        using namespace scheme::communication;
+        flatbuffers::FlatBufferBuilder builder;
+
+        /// Serialize the route
+        vector<scheme::cryptography::UUID> routeEntries;
+        for (auto hop : this->route)
+            routeEntries.push_back(hop.toScheme());
+
+        auto routeVector = builder.CreateVectorOfStructs(routeEntries);
+
+        /// Serialize the payload
+        auto payload = builder.CreateVector(this->payload);
+
+        /// Serialize the signature
+        // TODO Make this more memory efficient.
+        vector<uint8_t> signatureVector(this->signature.begin(), this->signature.end());
+        auto signature = builder.CreateVector(signatureVector);
+
+        auto message = CreateMessageDatagram(builder, routeVector, payload, signature);
+
+        /// Convert it to a byte array
+        builder.Finish(message, MessageDatagramIdentifier());
+        uint8_t *buf = builder.GetBufferPointer();
+
+        return {buf, buf + builder.GetSize()};
+    }
+
+    Result<Message, Message::MessageDeserializationError> Message::fromBuffer(vector<uint8_t> buffer) {
+
+        using namespace scheme::communication;
+
+        /// Verify the buffer type
+        if (!flatbuffers::BufferHasIdentifier(buffer.data(), MessageDatagramIdentifier()))
+            return Err(MessageDeserializationError::INVALID_IDENTIFIER);
+
+        /// Verify buffer integrity
+        auto verifier = flatbuffers::Verifier(buffer.data(), buffer.size());
+        if (!VerifyMessageDatagramBuffer(verifier))
+            return Err(MessageDeserializationError::INVALID_BUFFER);
+
+        auto msg = GetMessageDatagram(buffer.data());
+
+        /// Deserialize route
+        vector<cryptography::UUID> route;
+        auto routeBuffer = msg->route();
+        for (uint i = 0; i < routeBuffer->Length(); i++)
+            route.emplace_back(routeBuffer->Get(i));
+
+        /// Deserialize payload
+        vector<uint8_t> payload(msg->payload()->begin(), msg->payload()->end());
+
+        /// Deserialize signature
+        if (msg->signature()->size() != SIGNATURE_SIZE)
+            return Err(MessageDeserializationError::SIGNATURE_SIZE_MISMATCH);
+        SIGNATURE_T signature{};
+        // TODO Make this more memory efficient.
+        copy(msg->signature()->begin(), msg->signature()->end(), signature.begin());
+
+        return Ok(Message(route, payload, signature));
+    }
+
+#ifdef UNIT_TESTING
+
+    SCENARIO("Message exchange and verification",
+             "[unit_test][module][communication]") {
+
+        GIVEN("A message") {
+            cryptography::asymmetric::KeyPair keyPair(cryptography::asymmetric::generateKeyPair());
+            cryptography::UUID origin;
+            cryptography::UUID hop1;
+            cryptography::UUID hop2;
+            cryptography::UUID destination;
+
+            vector<cryptography::UUID> route = {hop1, hop2};
+
+            Message msg = Message::build({1, 2, 3}, origin, route, destination, keyPair);
+
+            WHEN("it is serialized") {
+                vector<uint8_t> serializedMsg = msg.serialize();
+
+                THEN("the serialized advertisement should contain an appropriate identifier") {
+                    REQUIRE(flatbuffers::BufferHasIdentifier(serializedMsg.data(),
+                                                             scheme::communication::MessageDatagramIdentifier()));
+                }
+
+                AND_WHEN("it is deserialized and reserialized again") {
+                    Message deserializedMsg = Message::fromBuffer(serializedMsg).unwrap();
+                    vector<uint8_t> reSerializedMsg = deserializedMsg.serialize();
+
+                    THEN("both bytestreams should be equal") {
+                        REQUIRE(reSerializedMsg == serializedMsg);
+                    }
+                }
+            }
+        }
+    }
+
+#endif
 }
--- modules/communication/Message.hpp
+++ modules/communication/Message.hpp
@@ -1,10 +1,44 @@
 #ifndef PROTOMESH_MESSAGE_HPP
 #define PROTOMESH_MESSAGE_HPP
 
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+#include "uuid.hpp"
+#include "asymmetric.hpp"
+
+#include "flatbuffers/flatbuffers.h"
+#include "communication/message_generated.h"
 
 namespace ProtoMesh::communication {
     class Message {
+        vector<cryptography::UUID> route;
+        vector<uint8_t> payload;
+        SIGNATURE_T signature;
 
+        enum class MessageDeserializationError {
+            INVALID_IDENTIFIER,
+            INVALID_BUFFER,
+            SIGNATURE_SIZE_MISMATCH
+        };
+
+        Message(vector<cryptography::UUID> route, vector<uint8_t> payload, SIGNATURE_T signature)
+                : route(std::move(route)), payload(std::move(payload)), signature(signature) {};
+
+    public:
+        static Message build(const vector<uint8_t> &payload, cryptography::UUID from, vector<cryptography::UUID> via,
+                             cryptography::UUID to, cryptography::asymmetric::KeyPair signer) {
+            via.insert(via.begin(), from);
+            via.push_back(to);
+            SIGNATURE_T signature(cryptography::asymmetric::sign(payload, signer.priv));
+            return Message(via, payload, signature);
+        }
+
+        vector<uint8_t> serialize();
+
+        static Result<Message, MessageDeserializationError> fromBuffer(vector<uint8_t> buffer);
     };
 }
 
