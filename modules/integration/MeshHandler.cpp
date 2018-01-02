d08b4a7a881963dba7993e194170d4a1a34985b2
Til Blechschmidt <til@blechschmidt.de>
2018-01-02 19:11:43 +0100

    Added integration module

---
+++ modules/integration/MeshHandler.cpp
@@ -0,0 +1,18 @@
+#ifdef UNIT_TESTING
+
+#include "catch.hpp"
+
+#endif
+
+#include "MeshHandler.hpp"
+
+namespace ProtoMesh {
+
+    // TODO Implement smth
+
+#ifdef UNIT_TESTING
+    SCENARIO("A top level API should exist", "[integration_test][module][integration]") {
+
+    }
+#endif // UNIT_TESTING
+}
---
+++ modules/integration/MeshHandler.hpp
@@ -0,0 +1,14 @@
+#ifndef PROTOMESH_PROTOMESH_HPP
+#define PROTOMESH_PROTOMESH_HPP
+
+namespace ProtoMesh {
+
+    class MeshHandler {
+
+    };
+
+}
+
+
+
+#endif //PROTOMESH_PROTOMESH_HPP
