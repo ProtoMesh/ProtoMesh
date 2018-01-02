cb240f26c7ae37491a8f275fe6f37ce86078a6b0
Til Blechschmidt <til@blechschmidt.de>
2018-01-02 21:05:16 +0100

    Added MeshHandler generateNew function.

--- modules/integration/MeshHandler.hpp
+++ modules/integration/MeshHandler.hpp
@@ -2,9 +2,11 @@
 #define PROTOMESH_PROTOMESH_HPP
 
 #include <utility>
-#include <RelativeTimeProvider.hpp>
 
-#include "TransmissionHandler.hpp"
+#include <uuid.hpp>
+#include <asymmetric.hpp>
+#include <TransmissionHandler.hpp>
+#include <RelativeTimeProvider.hpp>
 
 namespace ProtoMesh {
 
@@ -12,9 +14,19 @@ namespace ProtoMesh {
         TRANSMISSION_HANDLER_T transmissionHandler;
         REL_TIME_PROV_T timeProvider;
 
+        cryptography::UUID deviceID;
+        cryptography::asymmetric::KeyPair deviceKeys;
+
     public:
-        explicit MeshHandler(TRANSMISSION_HANDLER_T transmissionHandler, REL_TIME_PROV_T timeProvider)
-                : transmissionHandler(std::move(transmissionHandler)), timeProvider(std::move(timeProvider)) {};
+        explicit MeshHandler(cryptography::UUID deviceID, cryptography::asymmetric::KeyPair deviceKeys, TRANSMISSION_HANDLER_T transmissionHandler, REL_TIME_PROV_T timeProvider)
+                : transmissionHandler(std::move(transmissionHandler)), timeProvider(std::move(timeProvider)), deviceID(deviceID), deviceKeys(deviceKeys) {};
+
+        static MeshHandler generateNew(TRANSMISSION_HANDLER_T transmissionHandler, REL_TIME_PROV_T timeProvider) {
+            return MeshHandler(cryptography::UUID(),
+                               cryptography::asymmetric::generateKeyPair(),
+                               std::move(transmissionHandler),
+                               std::move(timeProvider));
+        }
     };
 
 }
